72db416bf02d150e3032325b4c68fbefe99ac966
Tiphaine <tiphaine.aubin28@hotmail.fr>
2025-02-04 12:49:33 +0100

    New CV

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,7 +3,7 @@ import { VerticalTimeline, VerticalTimelineElement, } from "react-vertical-timel
 import "react-vertical-timeline-component/style.min.css";
 import { FaGraduationCap, FaLaptopCode } from "react-icons/fa";
 import { MdOutlineContactPage, MdOutlineFileDownload } from "react-icons/md";
-import CV from '../assets/CV_TIPHAINE_AUBIN_2024.pdf';
+import CV from '../assets/CV25_AUBIN_Tiphaine.pdf';
 import "./style/Timeline.scss";
 
 // Chronologie verticale
